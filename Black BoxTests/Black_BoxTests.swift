0c43bcf242008240ac15fe4a1396fb256ad32fae
Jason Duffy <jasonsduffy@gmail.com>
2014-12-28 12:58:57 -0500

    Initial Commit

---
+++ Black BoxTests/Black_BoxTests.swift
@@ -0,0 +1,36 @@
+//
+//  Black_BoxTests.swift
+//  Black BoxTests
+//
+//  Created by Jason Duffy on 12/28/14.
+//  Copyright (c) 2014 Peapod Media, llc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Black_BoxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
