e685c63db74738cd97624087e1ca2bfd4556e4ef
Jason Duffy <jasonsduffy@gmail.com>
2015-01-22 00:05:12 -0500

    Direct hits UI work

---
+++ Black Box/EntryPoint.swift
@@ -0,0 +1,21 @@
+//
+//  EntryPoint.swift
+//  Black Box
+//
+//  Created by Jason Duffy on 1/21/15.
+//  Copyright (c) 2015 Peapod Media, llc. All rights reserved.
+//
+
+import SpriteKit
+
+class EntryPoint: SKSpriteNode {
+    var number = 0
+    
+    class func entryPoint(number: Int, imageNamed: String) -> EntryPoint {
+        let sprite = EntryPoint(imageNamed: imageNamed)
+        
+        sprite.number = number
+        sprite.name = "Entry\(number)"
+        return sprite
+    }
+}
--- Black Box/GameScene.swift
+++ Black Box/GameScene.swift
@@ -9,6 +9,27 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    let game = Game()
+    
+    func addEntryPoint(number: Int) {
+        let input = EntryPoint.entryPoint(number, imageNamed: "Hit")
+        switch number {
+        case 1...8:
+            input.position = CGPoint(x: 5, y: 689 - (number * 76))
+        case 9...16:
+            input.position = CGPoint(x: 81 + ((number - 9) * 76), y: 5)
+        case 17...24:
+            input.position = CGPoint(x: 689, y: 689 - ((25 - number) * 76))
+        case 25...32:
+            input.position = CGPoint(x: 81 + ((32 - number) * 76), y: 689)
+        default:
+            input.position = CGPoint(x: 0, y: 0)
+        }
+        input.anchorPoint = CGPoint(x: 0, y: 0)
+        input.hidden = true
+        addChild(input)
+    }
+    
     override init(size: CGSize) {
         super.init(size: size)
         
@@ -16,6 +37,13 @@ class GameScene: SKScene {
         background.position = CGPoint(x: 0, y: 0)
         background.anchorPoint = CGPoint(x: 0, y: 0)
         addChild(background)
+        
+        for i in 1...32 {
+            addEntryPoint(i)
+        }
+        
+        //TODO: real ball hiding
+        game.place(9)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -24,17 +52,23 @@ class GameScene: SKScene {
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        
-        
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
+            println("location: (\(location.x),\(location.y))")
             
             // do some stuff
+            if let entryPoint = self.nodeAtPoint(location) as? EntryPoint {
+                println(entryPoint.name!)
+                switch game.guess(entryPoint.number) {
+                case .Hit:
+                    entryPoint.hidden = false
+                default:
+                    entryPoint.hidden = true
+                }
+            }
         }
     }
    
