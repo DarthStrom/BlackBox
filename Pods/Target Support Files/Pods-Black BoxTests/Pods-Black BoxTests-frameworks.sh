4c0c9f6d249c8ca478b18e49e3270e993c744c68
Jason Duffy <jasonsduffy@gmail.com>
2019-06-15 11:12:23 -0400

    create development, staging, and production schemes

--- Pods/Target Support Files/Pods-Black BoxTests/Pods-Black BoxTests-frameworks.sh
+++ Pods/Target Support Files/Pods-Black BoxTests/Pods-Black BoxTests-frameworks.sh
@@ -160,12 +160,32 @@ strip_invalid_archs() {
 }
 
 
-if [[ "$CONFIGURATION" == "Debug" ]]; then
+if [[ "$CONFIGURATION" == "Debug-Production" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Moxie/Moxie.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
 fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
+if [[ "$CONFIGURATION" == "Debug-Development" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Moxie/Moxie.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
+fi
+if [[ "$CONFIGURATION" == "Debug-Staging" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Moxie/Moxie.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
+fi
+if [[ "$CONFIGURATION" == "Release-Production" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Moxie/Moxie.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
+fi
+if [[ "$CONFIGURATION" == "Release-Staging" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Moxie/Moxie.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
+fi
+if [[ "$CONFIGURATION" == "Release-Development" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Moxie/Moxie.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
